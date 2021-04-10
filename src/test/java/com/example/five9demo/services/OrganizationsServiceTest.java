9084d4d6393c84ee758ca4eec3079aa5d6f027a0
Walid Moustafa <waleedaleem@hotmail.com>
2021-04-10 13:39:08 +1000

    Parameterize the unit test to cover more negative test cases and update test to fail invalid organization name value

--- src/test/java/com/example/five9demo/services/OrganizationsServiceTest.java
+++ src/test/java/com/example/five9demo/services/OrganizationsServiceTest.java
@@ -6,14 +6,24 @@ import com.example.five9demo.requests.OrganizationRequest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.fail;
 
 class OrganizationServiceTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(OrganizationServiceTest.class);
+
     @Mock
     OrganizationRepository organizationRepository;
     @InjectMocks
@@ -47,15 +57,22 @@ class OrganizationServiceTest {
         }
     }
 
-    @Test
-    void testSaveAllOrganizationsInvalidOrgName() throws Exception {
+    static Stream<String> InvalidOrganizationNames() {
+        return Stream.of("", "   ", null);
+    }
+
+    @ParameterizedTest
+    @MethodSource("InvalidOrganizationNames")
+    void testSaveAllOrganizationsInvalidOrgName(String name) throws Exception {
+        logger.info("testing with name = \"{}\"", name);
         try{
-            this.organization.setName("");
+            this.organization.setName(name);
             this.organizations.add(organization);
             this.organizationRequest.setOrganizations(organizations);
             organizationsService.saveAllOrganizations(organizationRequest);
+            fail("Should not have allowed invalid organization names");
         } catch (Exception e){
-            Assertions.assertEquals(e.getMessage(),"Organization name is invalid");
+            Assertions.assertEquals("Organization name is invalid", e.getMessage());
         }
     }
 
