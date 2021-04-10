46306a99b01a1d6d743183788520aecd868b4128
Walid Moustafa <waleedaleem@hotmail.com>
2021-04-10 16:51:34 +1000

    Add a new customer repository

---
+++ src/main/java/com/example/five9demo/repositories/CustomerRepository.java
@@ -0,0 +1,11 @@
+package com.example.five9demo.repositories;
+
+import com.example.five9demo.data.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Set;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+
+    Set<Customer> findByOrganizationName(String organizationName);
+}
