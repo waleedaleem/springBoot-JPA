cd04ceb8f685ff986b28d5fe3fe0c0394ddd5f1a
Walid Moustafa <waleedaleem@hotmail.com>
2021-04-11 11:22:37 +1000

    Mark entity names as unique and uppercase them in database

--- src/main/java/com/example/five9demo/data/Customer.java
+++ src/main/java/com/example/five9demo/data/Customer.java
@@ -1,5 +1,6 @@
 package com.example.five9demo.data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -10,24 +11,35 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.querydsl.core.annotations.QueryEntity;
 
 import lombok.Data;
+
+import java.io.Serializable;
 
 @Data
 @Entity
 @Table(name = "customers")
 @QueryEntity
-public class Customer {
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Customer implements Serializable {
+
+    private static final long serialVersionUID = -6433721069248439325L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @JsonIgnore
     private Integer id;
 
+    @Column(unique = true)
     private String name;
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "organization_name", nullable = false, referencedColumnName = "name")
     private Organization organization;
+
+    public void setName(String name) {
+        this.name = name.toUpperCase();
+    }
 }
--- src/main/java/com/example/five9demo/data/Organization.java
+++ src/main/java/com/example/five9demo/data/Organization.java
@@ -1,18 +1,26 @@
 package com.example.five9demo.data;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.querydsl.core.annotations.QueryEntity;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
 @Data
+@EqualsAndHashCode(exclude = "customers")
 @Entity
 @Table(name = "organizations")
 @QueryEntity
-public class Organization {
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Organization implements Serializable {
+
+    private static final long serialVersionUID = -6433721069248439324L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @JsonIgnore
@@ -22,5 +30,10 @@ public class Organization {
     private String name;
 
     @OneToMany(mappedBy = "organization", fetch = FetchType.LAZY)
+    @JsonIgnore
     private Set<Customer> customers = new HashSet<>();
+
+    public void setName(String name) {
+        this.name = name.toUpperCase();
+    }
 }
