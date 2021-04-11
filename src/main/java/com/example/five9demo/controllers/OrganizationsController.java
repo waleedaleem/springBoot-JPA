3140a4fb193af95823502e669879ce2410a7c88e
Walid Moustafa <waleedaleem@hotmail.com>
2021-04-11 15:09:11 +1000

    Add a new end point to update organization's customers

--- src/main/java/com/example/five9demo/controllers/OrganizationsController.java
+++ src/main/java/com/example/five9demo/controllers/OrganizationsController.java
@@ -1,5 +1,6 @@
 package com.example.five9demo.controllers;
 
+import com.example.five9demo.DTO.CustomerDTO;
 import com.example.five9demo.requests.OrganizationRequest;
 import com.example.five9demo.responses.OrganizationResponse;
 import com.example.five9demo.services.OrganizationsService;
@@ -8,13 +9,18 @@ import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.constraints.NotBlank;
 import javax.ws.rs.core.Context;
 
 @RestController
@@ -22,12 +28,15 @@ import javax.ws.rs.core.Context;
 @Api(tags = {"Organization Services"})
 public class OrganizationsController {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(OrganizationsController.class);
+    private static final Logger logger = LoggerFactory.getLogger(OrganizationsController.class);
 
     @Autowired
     private OrganizationsService organizationsService;
 
-    @PostMapping(path = "/organizations")
+    @PostMapping(
+            path = "/organizations",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     @ApiOperation(tags = "Organization Services", value ="Add organizations", notes = "This API will create/add organizations")
     public ResponseEntity<?> addOrganizations(@Context HttpServletRequest request, @RequestBody OrganizationRequest organizationRequest){
 
@@ -41,6 +50,35 @@ public class OrganizationsController {
         return ResponseEntity.ok(organizationResponse);
     }
 
+    @PutMapping(
+            path = "/organizations/{organizationName}/customers/{customerName}",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(
+            tags = "Organization Services",
+            value = "Updates an organization customer",
+            notes = "This API will update an existing organization customer")
+    public ResponseEntity<OrganizationResponse> updateOrganizationCustomer(@Context HttpServletRequest request,
+                                                                           @PathVariable @NotBlank String organizationName,
+                                                                           @PathVariable @NotBlank String customerName,
+                                                                           @RequestBody CustomerDTO updatedCustomer) {
+        if (logger.isDebugEnabled()) {
+            logger.debug(
+                    "Received updateOrganizationCustomer request {}", updatedCustomer.toJson());
+        }
 
+        organizationsService.updateOrganizationCustomer(
+                organizationName, customerName, updatedCustomer);
+        OrganizationResponse organizationResponse = new OrganizationResponse(
+                HttpStatus.OK.getReasonPhrase(), String.valueOf(HttpStatus.OK.value()),
+                "customer updated");
+        ResponseEntity<OrganizationResponse> response = ResponseEntity.ok(organizationResponse);
 
+        if (logger.isDebugEnabled()) {
+            logger.debug(
+                    "Processed updateOrganizationCustomer request successfully. Response: {}",
+                    organizationResponse.toJson());
+        }
+        return response;
+    }
 }
--- src/main/java/com/example/five9demo/responses/OrganizationResponse.java
+++ src/main/java/com/example/five9demo/responses/OrganizationResponse.java
@@ -1,10 +1,31 @@
 package com.example.five9demo.responses;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_EMPTY;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrganizationResponse {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper().setSerializationInclusion(
+            NON_EMPTY).disable(SerializationFeature.INDENT_OUTPUT);
+
     private String message;
     private String messageCode;
     private String information;
+
+    public String toJson() {
+        try {
+            return objectMapper.writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            return this.toString();
+        }
+    }
 }
